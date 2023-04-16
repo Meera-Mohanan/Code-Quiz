092d2fdee7393c260dc574bc76c027e169f82e8a
Meera-Mohanan <126405858+Meera-Mohanan@users.noreply.github.com>
2023-04-16 15:27:38 +0800

    js updated

---
+++ JS/questions.js
@@ -0,0 +1,22 @@
+var questions= [
+    {
+        title:"Where is the correct place to insert a JavaScript?",
+        choices : ['The<head> section','Both the <head> section and the <body> section are correct','The <bosy> section'],
+        answer : 'Both the <head> section and the <body> section are correct  '
+    },
+    {
+        title: "How do you create a function in JavaScript?",
+        choices: ['funtion:myFunction()','function myFunction()','function = myFunction()'],
+        answer : 'function myFunction()'
+    },
+    {
+        title: "How to write an IF statement in JavaScript?",
+        choices: ['if i==5 then','if i=5','if (i==5)','if i = 5 then'],
+        answer : 'if (i == 5)'
+    },
+    {
+        title: "How does a WHILE loop start?",
+        choices: ['while i = 1 to 10','while (i<=10;i++)','while(i<=10)'],
+        answer : 'while (i <= 10)'
+    }
+]
